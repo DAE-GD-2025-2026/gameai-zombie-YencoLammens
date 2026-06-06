2c46b4c921cc0558ddf7dccce18b77c13e716adc
Yenco Lammens <161343595+YencoLammens@users.noreply.github.com>
2026-06-06 22:24:40 +0200

    Update survivor percepter with perception binding and blackboard updates

--- Plugins/GameFeatures/LammensYencoZombie/Source/LammensYencoZombieRuntime/StudentPerceptorLammensYenco.cpp
+++ Plugins/GameFeatures/LammensYencoZombie/Source/LammensYencoZombieRuntime/StudentPerceptorLammensYenco.cpp
@@ -1,24 +1,157 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "StudentPerceptorLammensYenco.h"
+#include "AIController.h"
+#include "GameFramework/Pawn.h"
+#include "SurvivorAPIHelpersLammensYenco.h"
+
+namespace BBKeys
+{
+    const FName NearestZombie = TEXT("NearestZombie");
+    const FName NearestWeapon = TEXT("NearestWeapon");
+    const FName NearestItem = TEXT("NearestItem");
+    const FName NearestHouse = TEXT("NearestHouse");
+    const FName NearestPurgeZone = TEXT("NearestPurgeZone");
+    const FName bIsInPurgeZone = TEXT("bIsInPurgeZone");
+    const FName bHasWeapon = TEXT("bHasWeapon");
+    const FName HealthValue = TEXT("HealthValue");
+    const FName EnergyValue = TEXT("EnergyValue");
+}
 
 UStudentPerceptorLammensYenco::UStudentPerceptorLammensYenco()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.TickInterval = 0.1f;
 }
 
 void UStudentPerceptorLammensYenco::BeginPlay()
 {
-	Super::BeginPlay();
-	
-	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
-	{
-		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptorLammensYenco::OnPerceptionUpdated);
-	}
+    Super::BeginPlay();
+    TryBindPerception();
+}
+
+void UStudentPerceptorLammensYenco::TryBindPerception()
+{
+    if (bPerceptionBound) return;
+
+    UAIPerceptionComponent* PC = GetOwner()->GetComponentByClass<UAIPerceptionComponent>();
+
+    if (!PC)
+    {
+        APawn* Pawn = Cast<APawn>(GetOwner());
+        if (Pawn)
+        {
+            if (AAIController* AIC = Cast<AAIController>(Pawn->GetController()))
+                PC = AIC->GetComponentByClass<UAIPerceptionComponent>();
+        }
+    }
+    if (PC)
+    {
+        PC->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptorLammensYenco::OnPerceptionUpdated);
+        bPerceptionBound = true;
+    }
+}
+
+void UStudentPerceptorLammensYenco::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+ 
+    TryBindPerception();
+ 
+    VisibleZombies.RemoveAll([](const TObjectPtr<AActor>& A) { return !IsValid(A); });
+    KnownItems.RemoveAll([](const TObjectPtr<AActor>& A) { return !IsValid(A); });
+    KnownWeapons.RemoveAll([](const TObjectPtr<AActor>& A) { return !IsValid(A); });
+    KnownHouses.RemoveAll([](const TObjectPtr<AActor>& A) { return !IsValid(A); });
+    ActivePurgeZones.RemoveAll([](const TObjectPtr<AActor>& A) { return !IsValid(A); });
+ 
+    const FVector OwnerPos = GetOwner()->GetActorLocation();
+    KnownHouses.RemoveAll([&](const TObjectPtr<AActor>& A) {
+        return !IsValid(A) || FVector::DistSquared(OwnerPos, A->GetActorLocation()) < FMath::Square(150.f);
+    });
+ 
+    UpdateBlackboardKeys();
 }
 
 void UStudentPerceptorLammensYenco::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
-		FString::Printf(TEXT("Saw Something!")));
+    if (!IsValid(Actor)) return;
+
+    const bool bSensed = Stimulus.WasSuccessfullySensed();
+
+    if (IsZombie(Actor))
+    {
+        if (bSensed) VisibleZombies.AddUnique(Actor);
+        else VisibleZombies.Remove(Actor);
+    }
+    else if (IsWeapon(Actor))
+    {
+        if (bSensed) KnownWeapons.AddUnique(Actor);
+        else KnownWeapons.Remove(Actor);
+    }
+    else if (IsItem(Actor))
+    {
+        if (bSensed) KnownItems.AddUnique(Actor);
+    }
+    else if (IsHouse(Actor))
+    {
+        if (bSensed) KnownHouses.AddUnique(Actor);
+    }
+    else if (IsPurgeZone(Actor))
+    {
+        if (bSensed) ActivePurgeZones.AddUnique(Actor);
+        else ActivePurgeZones.Remove(Actor);
+    }
+
+    UpdateBlackboardKeys();
 }
+
+UBlackboardComponent* UStudentPerceptorLammensYenco::GetBlackboard()
+{
+    if (BlackboardComp) return BlackboardComp;
+
+    if (APawn* Pawn = Cast<APawn>(GetOwner()))
+        if (AAIController* AIC = Cast<AAIController>(Pawn->GetController()))
+            BlackboardComp = AIC->GetBlackboardComponent();
+
+    return BlackboardComp;
+}
+
+void UStudentPerceptorLammensYenco::UpdateBlackboardKeys()
+{
+    UBlackboardComponent* BB = GetBlackboard();
+    if (!BB) return;
+
+    BB->SetValueAsObject(BBKeys::NearestZombie, FindNearest(VisibleZombies));
+    BB->SetValueAsObject(BBKeys::NearestWeapon, FindNearest(KnownWeapons));
+    BB->SetValueAsObject(BBKeys::NearestItem, FindNearest(KnownItems));
+    BB->SetValueAsObject(BBKeys::NearestHouse, FindNearest(KnownHouses));
+    BB->SetValueAsObject(BBKeys::NearestPurgeZone, FindNearest(ActivePurgeZones));
+    BB->SetValueAsBool(BBKeys::bIsInPurgeZone, !ActivePurgeZones.IsEmpty());
+
+    if (APawn* Pawn = Cast<APawn>(GetOwner()))
+    {
+        BB->SetValueAsFloat(BBKeys::HealthValue, SurvivorAPI::GetHealthRatio(Pawn));
+        BB->SetValueAsFloat(BBKeys::EnergyValue, SurvivorAPI::GetStaminaRatio(Pawn));
+    }
+}
+
+AActor* UStudentPerceptorLammensYenco::FindNearest(const TArray<TObjectPtr<AActor>>& Actors) const
+{
+    AActor* Nearest  = nullptr;
+    float BestDist = FLT_MAX;
+    const FVector OwnerPos = GetOwner()->GetActorLocation();
+
+    for (const TObjectPtr<AActor>& Actor : Actors)
+    {
+        if (!IsValid(Actor)) continue;
+        const float Dist = FVector::DistSquared(OwnerPos, Actor->GetActorLocation());
+        if (Dist < BestDist) { BestDist = Dist; Nearest = Actor; }
+    }
+    return Nearest;
+}
+
+bool UStudentPerceptorLammensYenco::IsZombie(AActor* A) const { return A->GetClass()->GetName().Contains(TEXT("Zombie")); }
+bool UStudentPerceptorLammensYenco::IsWeapon(AActor* A) const { const FString N = A->GetClass()->GetName(); return N.Contains(TEXT("Pistol")) || N.Contains(TEXT("Shotgun")); }
+bool UStudentPerceptorLammensYenco::IsItem(AActor* A) const { return A->GetClass()->GetName().Contains(TEXT("Item")); }
+bool UStudentPerceptorLammensYenco::IsHouse(AActor* A) const { return A->GetClass()->GetName().Contains(TEXT("House")); }
+bool UStudentPerceptorLammensYenco::IsPurgeZone(AActor* A) const { return A->GetClass()->GetName().Contains(TEXT("Purge")); }
--- Plugins/GameFeatures/LammensYencoZombie/Source/LammensYencoZombieRuntime/StudentPerceptorLammensYenco.h
+++ Plugins/GameFeatures/LammensYencoZombie/Source/LammensYencoZombieRuntime/StudentPerceptorLammensYenco.h
@@ -8,6 +8,7 @@
 #include "Perception/AISenseConfig_Sight.h"
 #include "Perception/AISenseConfig_Damage.h"
 #include "Perception/AISense_Damage.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "StudentPerceptorLammensYenco.generated.h"
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -17,9 +18,42 @@ class LAMMENSYENCOZOMBIERUNTIME_API UStudentPerceptorLammensYenco : public UActo
 
 public:
 	UStudentPerceptorLammensYenco();
-	
-	virtual void BeginPlay() override;
 
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void TryBindPerception();
 	UFUNCTION()
 	virtual void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+private:
+	UPROPERTY()
+	TObjectPtr<UBlackboardComponent> BlackboardComp;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> VisibleZombies;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> KnownItems;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> KnownWeapons;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> KnownHouses;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> ActivePurgeZones;
+
+	UBlackboardComponent* GetBlackboard();
+	void UpdateBlackboardKeys();
+	AActor* FindNearest(const TArray<TObjectPtr<AActor>>& Actors) const;
+
+	bool IsZombie(AActor* Actor) const;
+	bool IsWeapon(AActor* Actor) const;
+	bool IsItem(AActor* Actor) const;
+	bool IsHouse(AActor* Actor) const;
+	bool IsPurgeZone(AActor* Actor) const;
+	
+	bool bPerceptionBound = false;
 };
